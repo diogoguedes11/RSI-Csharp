3106fc984cafe9ddd3e0fac76d6402d83a766778
Sandro Ferreira <sandro_ferreira@outlook.pt>
2019-02-11 13:07:37 +0000

    Delegates

---
+++ Projects/Console Apps/Delegates/Delegates/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Carro
+    {
+        public event OnExcesso_Handler OnExcesso;
+
+        public int velocidade;
+
+        public void Acelerar(int quantidade)
+        {
+            velocidade = velocidade + quantidade;
+            if (velocidade > 120)
+                OnExcesso(this, new EventArgs());
+        }
+        public void Travar(int quantidade)
+        {
+            velocidade = velocidade - quantidade;
+        }
+    }
+}
---
+++ Projects/Console Apps/Delegates/Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void OnExcesso_Handler(object sender, EventArgs e);
+    // Um delegate é um apontador de métodos.
+    // Um evento é um delegate com o return type void.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo k;
+            Carro c1 = new Carro();
+
+            c1.OnExcesso += C1_OnExcesso;
+
+            do
+            {
+                k = Console.ReadKey(true);
+                switch (k.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        c1.Acelerar(5);
+                        Console.WriteLine("Velocidade: {0}", c1.velocidade);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        c1.Travar(5);
+                        Console.WriteLine("Velocidade: {0}", c1.velocidade);
+                        break;
+                }
+            }
+            while (k.Key != ConsoleKey.Escape);
+
+            Console.Read();
+        }
+
+        private static void C1_OnExcesso(object sender, EventArgs e)
+        {
+            Console.WriteLine("Excesso de Velocidade!");
+        }
+    }
+}
